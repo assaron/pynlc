5a77b121721fb468acd71cee34cd43bc7b194ddb
Aleksey Sergushichev <alsergbox@gmail.com>
2010-10-05 00:40:17 +0400

    Fixed previous revision (forgotten file)

--- board.py
+++ board.py
@@ -23,54 +23,9 @@ from threading import Condition
 from Queue import Queue
 
 from util import nsplit, get_expiration_day, logobject
-from node import *
+from nodes import *
 import config
 
-MESSAGE_PROPERTIES = (
-    ("id", int_decoder), ("unknown1", int_decoder), ("parent_id", int_decoder),
-    ("delete_", int_decoder), ("IP", str_decoder), ("hostname", unicode_decoder),
-    ("nick", unicode_decoder), ("body", unicode_decoder),
-    ("edit_time", time_decoder), ("channel_id", int_decoder),
-    ("unknown2", int_decoder), ("mac", str_decoder), ("zero1", int_decoder),
-    ("zero2", int_decoder), ("zero3", int_decoder), ("zero4", int_decoder),
-    ("time_id", int_decoder), ("deleted", int_decoder), ("post_time", time_decoder)
-    )
-
-# Message base class
-MessageBase = Node(MESSAGE_PROPERTIES)
-
-class Message(MessageBase):
-    """
-        Message.
-
-        zeroX     - fields that seems to be always zero
-        thread_id - something like thread id
-        time_id   - consecutive ID: almost all the time 
-                    (post_time1 > post_time2) == (time_id1 > time_id2)
-        delete_   - seems to be equal to one after deleting
-        deleted   - deletes message from original client (oppose to delete_)
-    """
-
-    def __init__(self, messages_update):
-        """
-            Parses message_update.
-        """
-        MessageBase.__init__(self, messages_update)
-        self._properties["body"] = self._properties["body"].replace("\x01", "\n")
-
-CHANNEL_PROPERTIES = (("id", int_decoder),
-                      ("name", unicode_decoder),
-                      ("description", unicode_decoder))
-
-# Channel base class
-ChannelBase = Node(CHANNEL_PROPERTIES)
-
-class Channel(ChannelBase):
-    """
-        Channel.
-    """
-    def __init__(self, channel_update):
-        ChannelBase.__init__(self, channel_update)
 
 class Board:
     """
@@ -280,7 +235,7 @@ class Board:
                 old_message = self._messages[message.id()]
                 parent.replace_reply(old_message, message)
                 # moving replies from old message to the new one
-                mesage._replies = old_message._replies
+                message._replies = old_message._replies
                 old_message._replies = []
             else:
                 # new message
